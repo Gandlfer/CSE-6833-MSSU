73c179d05fffcf5cd0f8b7f3be0f3754a4902897
CawFee <darryllee1999@gmail.com>
2023-02-07 05:42:29 +0800

    Main driver for multiplication.py

---
+++ Assignment-1/integer-multiplication-comparison/main.py
@@ -0,0 +1,28 @@
+from multiplication import *
+import random as rand
+import timeit
+import matplotlib.pyplot as plt
+
+power=1
+y1 = list()
+y2 = list()
+x = list()
+while power<=20:
+    x.append(power)
+    num= lambda: rand.randint(pow(10,power-1),pow(10,power)-1)
+    karatsuba=timeit.Timer(lambda: karatsubaMultiplication(num(),num())).timeit(1)
+    gradeSchool=timeit.Timer(lambda: gradeSchoolMultiplication(num(),num())).timeit(1)
+    y1.append(karatsuba)
+    y2.append(gradeSchool)
+    power=power+1
+
+plt.plot(x, y1, label="Karatsuba")
+plt.plot(x, y2, label="Grade School")
+plt.xlabel("Length of number")
+plt.ylabel("Execution Time")
+# show a legend on the plot
+plt.legend()
+
+# function to show the plot
+plt.show()
+
